5aa89bf7d34d19bd6c6c3a6ee103042d3a8269b2
Zachery Moneypenny <whazzmaster@gmail.com>
2017-10-27 16:32:40 -0500

    WIP: Builds out PhoneList agent

--- agents/phone_list/lib/phone_list.ex
+++ agents/phone_list/lib/phone_list.ex
@@ -1,18 +1,24 @@
 defmodule PhoneList do
+  use Agent
   @moduledoc """
   Documentation for PhoneList.
   """
 
-  @doc """
-  Hello world.
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
 
-  ## Examples
+  def add_entry(phone_book, name, phone) do
+    case Agent.update(phone_book, &Map.put(&1, name, phone)) do
+      :ok -> {:ok, %{name: name, phone: phone}}
+      _ -> {:error, "Error updating"}
+    end
+  end
 
-      iex> PhoneList.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def search(phone_book, search_term) do
+    case Agent.get(phone_book, &Map.get(&1, search_term)) do
+      nil -> {:error, "Search term invalid: #{search_term}"}
+      phone -> {:ok, %{name: search_term, phone: phone}}
+    end
   end
 end
--- agents/phone_list/test/phone_list_test.exs
+++ agents/phone_list/test/phone_list_test.exs
@@ -1,8 +1,40 @@
 defmodule PhoneListTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
   doctest PhoneList
 
-  test "greets the world" do
-    assert PhoneList.hello() == :world
+  setup do
+    {:ok, phone_book} = start_supervised(PhoneList)
+    %{phone_book: phone_book}
+  end
+
+  test "add_entry adds to the phone book", %{phone_book: phone_book} do
+    # assert phone list dot add entry, passing phone book
+    assert(
+      {:ok, %{name: "Zach", phone: "555-123-4567"}}
+      ==
+      PhoneList.add_entry(phone_book, "Zach", "555-123-4567")
+    )
+    # a name and a phone number, returns tuple {:ok, name, phone}
+  end
+
+  test "search looks up an entry in the phone book", %{phone_book: phone_book} do
+    # add entry to phone book
+    # assert that a search for entry returns the name and number
+    PhoneList.add_entry(phone_book, "Pat", "555-765-4321")
+
+    assert(
+      {:ok, %{name: "Pat", phone: "555-765-4321"}}
+      ==
+      PhoneList.search(phone_book, "Pat")
+    )
   end
 end
+
+# {:ok, name, phone}
+# {:ok, %{name: name, phone: phone}}
+# {:error, "this is bad"}
+
+# mix in agent module to phone list
+# make an agent
+# pass pid to test cases
